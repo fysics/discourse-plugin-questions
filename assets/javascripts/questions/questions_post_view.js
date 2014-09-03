e55369967461e6f2f3e50aa7e8fe447da96de748
Zane Beckman <fysics@users.noreply.github.com>
2014-09-03 10:31:51 -0600

    remove urlForPostNumber
    Temporarily pull urlForPostNumber from questions_post_view.js

--- assets/javascripts/questions/questions_post_view.js
+++ assets/javascripts/questions/questions_post_view.js
@@ -7,12 +7,14 @@ Discourse.Topic.reopen({
         return data;
     }.property(),
 
+/*
     urlForPostNumber: function(post_number) {
         // on questions, the posts order changes and therefore the links
         // using counters are broken. Don't pretend this
         if (!this.get("isQuestion")) return this._super(post_number);
         return this.get("url");
     },
+*/
 
     isQuestion: Em.computed.equal('archetype', 'question')
 });
